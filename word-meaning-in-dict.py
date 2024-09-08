db22155b726e4e881b14fba8b3b05dc1cf5ecaab
Shashikant Sharma <159624249+shashikant-sharma71@users.noreply.github.com>
2024-09-09 00:17:56 +0530

    Add files via upload

---
+++ word-meaning-in-dict.py
@@ -0,0 +1,12 @@
+# Write a program to create a dictonary of hindi with values as their English translation. Provide user with an option to look it up!
+
+dicts={
+    "Madad":"Help",
+    "Garib":"Poor",
+    "Ameer":"Rich",
+    "Mahaan":"Great"
+}
+
+dict=input("Enter The word in Hindi ")
+
+print(dicts[dict])
